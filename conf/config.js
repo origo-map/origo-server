21d9d3aa3cfcffb27727152f6c20b1b55f82d444
tonnyandersson <tonnyandersson@users.noreply.github.com>
2019-07-08 11:27:42 +0200

    Add mapstate

--- conf/config.js
+++ conf/config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  mapState: {
+    'storagePath': 'OrigoMapState' // Path to map state storage. Current path will save map state to a folder named OrigoMapState in the origo-server directory.
+  },
   getInskrivning: {
     url: 'https://services.lantmateriet.se/distribution/produkter/inskrivning/v2.1',
     user: 'xxxxx',
