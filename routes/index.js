7b759635915ec9eabfc98eeab8f428bad7986f3f
Johnny Blästa <johnny.blasta@sundsvall.se>
2020-02-03 12:15:30 +0100

    Changed the routing and added new routes.

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,9 @@ var lmProxyVer = require('../handlers/lmproxyver');
 var getAkt = require('../handlers/getakt');
 var lmElevation = require('../handlers/lmelevation');
 var lmSearchPlacename = require('../handlers/lmsearchplacename');
-var lmSearchEstate = require('../handlers/lmSearchEstate');
+var lmEstate = require('../handlers/lmsearchestate');
 var lmSearchAddress = require('../handlers/lmsearchaddress');
+var lmGetEstate = require('../handlers/lmgetestate');
 
 /* GET start page. */
 router.get('/', function (req, res) {
@@ -28,9 +29,11 @@ router.all('/proxy', proxy);
 router.all('/lmproxy/*', lmProxy);
 router.all('/lmproxy-ver/*', lmProxyVer);
 router.all('/document/*', getAkt);
-router.all('/lmelevation*', lmElevation);
-router.all('/lmsearchplacename*', lmSearchPlacename);
-router.all('/lmregisterenheter*', lmSearchEstate);
-router.all('/lmsearchaddress*', lmSearchAddress);
+router.all('/lm/elevation*', lmElevation);
+router.all('/lm/placenames*', lmSearchPlacename);
+router.all('/lm/enhetsomraden*', lmEstate['lmGetEstateFromPoint']);
+router.all('/lm/registerenheter*', lmEstate['lmSearchEstate']);
+router.all('/lm/addresses*', lmSearchAddress);
+router.all('/lm/getestate*', lmGetEstate);
 
 module.exports = router;
