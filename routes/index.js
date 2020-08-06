2f256a6b14a5b66311afd1e99a51bf875c31c820
Johnny Blästa <johnny.blasta@sundsvall.se>
2020-08-06 12:23:47 +0200

    Initial commit of ConvertToGeojson

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ var lmSearchAddress = require('../handlers/lmsearchaddress');
 var lmGetEstate = require('../handlers/lmgetestate');
 var iotProxy = require('../handlers/iotproxy');
 var overpass = require('../handlers/overpass');
+var convertToGeojson = require('../handlers/converttogeojson');
 
 /* GET start page. */
 router.get('/', function (req, res) {
@@ -43,5 +44,6 @@ router.all('/lm/addresses*', lmSearchAddress);
 router.all('/lm/getestate*', lmGetEstate);
 router.all('/iotproxy/*', iotProxy);
 router.all('/overpass/*', overpass);
+router.all('/converttogeojson/*', convertToGeojson);
 
 module.exports = router;
