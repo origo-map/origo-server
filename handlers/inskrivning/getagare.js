2f22b57972670e50a4c9760d5ef9dd3c850c90c2
asemoller <ase.moller@sigtuna.se>
2020-11-12 09:42:54 +0100

    Cleaning up

--- handlers/inskrivning/getagare.js
+++ handlers/inskrivning/getagare.js
@@ -57,9 +57,7 @@ module.exports = function getAgare(prop, data, model) {
   }
 
   function getOrg(orgObj) {
-    var namn = {};
-    var orgnamn = objectifier.get(ns + ':organisationsnamn', orgObj);
-    namn.namn = orgnamn;
+    var namn = getNamn(orgObj);
     var utlandsk = objectifier.get(ns + ':Utlandsadress', orgObj);
     var adress = getAdress(orgObj[ns + ':Adress']);
     if (utlandsk) {
