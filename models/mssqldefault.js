0aa4f66a679db33b64fb9038164f6c8df437ca39
MattiasSp <mattias.spangmyr@gmail.com>
2022-01-14 14:29:25 +0100

    Handle custom limit in query parameter

--- models/mssqldefault.js
+++ models/mssqldefault.js
@@ -1,4 +1,4 @@
-var mssqlDefault = function mssqlDefault(queryString, queryOptions, defaultLimit) {
+var mssqlDefault = function mssqlDefault(queryString, queryOptions) {
   var schema = queryOptions.schema;
   var database = queryOptions.database;
   var table = queryOptions.table;
@@ -13,8 +13,7 @@ var mssqlDefault = function mssqlDefault(queryString, queryOptions, defaultLimit
   var type = " '" + table + "'" + " AS TYPE, ";
   var condition = queryString;
   var searchString;
-  var limitNumber = queryOptions.limit || defaultLimit || 1000;
-  var limit = "TOP " + limitNumber.toString() + " ";
+  var limit = queryOptions.limit ? "TOP " + queryOptions.limit.toString() + " " : "";
 
   searchString =
     "SELECT " + limit +
@@ -24,6 +23,6 @@ var mssqlDefault = function mssqlDefault(queryString, queryOptions, defaultLimit
     " ORDER BY " + searchField + "";
 
   return searchString;
-}
+};
 
 module.exports = mssqlDefault;
