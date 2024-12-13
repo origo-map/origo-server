f2c1de6f6a011178faeb13f07e8e05cf96ad0c8b
Johnny Blästa <johnny.blasta@sundsvall.se>
2024-12-13 12:35:28 +0100

    dep: remove request as dependecy in favour of axios, added documentation

--- handlers/lmelevation.js
+++ handlers/lmelevation.js
@@ -1,112 +1,111 @@
 var conf = require('../conf/config');
-var request = require('request');
-var rp = require('request-promise');
+var axios = require('axios');
+const lmtokenhandler = require('./lmtokenhandler');
 var transformCoordinates = require('../lib/utils/transformcoordinates');
-//var Promise = require('bluebird');
 
-var objectIds;
-var username;
-var password;
 var srid = '3006';
 var validProjs = ["3006", "3007", "3008", "3009", "3010", "3011", "3012", "3013", "3014", "3015", "3016", "3017", "3018", "3857", "4326"];
 
-// Token holder
-let token;
-let scope;
-var proxyUrl = 'lmelevation';
+var confObj = 'lmelevation';
 
-// Do the request in proper order
+/**
+ * Main function to process elevation requests.
+ * Handles token retrieval and directing request handling based on method (GET/POST).
+ *
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ */
 const lmElevation = async (req, res) => {
-  if (conf[proxyUrl]) {
-    configOptions = Object.assign({}, conf[proxyUrl]);
-    scope = configOptions.scope;
-
-    // Get a token from LM
-    await getTokenAsyncCall(configOptions.consumer_key, configOptions.consumer_secret, configOptions.scope);
-
-    // Do a POST with all the IDs from free search to get the complete objects with geometry
-    await doGetAsyncCall(req, res, configOptions, proxyUrl);
+  if (conf[confObj]) {
+    const config = Object.assign({}, conf[confObj]);
+    try {
+      // Retrieve access token from Lantmäteriet using the helper module for token handling
+      const tokenObject = await lmtokenhandler({
+        id: confObj,
+        url_token: config.url_token,
+        url_revoke: config.url_revoke,
+        consumer_key: config.consumer_key,
+        consumer_secret: config.consumer_secret,
+        scope: config.scope
+      });
+      // Process GET or POST requests for elevation data
+      await doGetAsyncCall(req, res, config, tokenObject.token);
+    } catch (error) {
+      console.log('Error:', error);
+      res.status(500).send({ 'error': error.message });
+    }
 
   }
 }
 
-// Export the module
-module.exports = lmElevation;
-
-function getTokenWait(options) {
-  // Return promise to be invoked for authenticating on service requests
-  return new Promise((resolve, reject) => {
-      // Requesting the token service object
-      request(options, (error, response, body) => {
-          if (error) {
-            console.log('Error token:' + error);
-            reject('An error occured collecting token: ', error);
-          } else {
-            token = body.access_token;
-            // console.log('Got token ' + token);
-            resolve(body.access_token);
-          }
-      })
-  })
-}
-
-async function getTokenAsyncCall(consumer_key, consumer_secret, scope) {
-  // Request a token from Lantmateriet API
-  const options = {
-      url: configOptions.url_token,
-      method: 'POST',
-      headers: {
-         'Authorization': 'Basic ' + Buffer.from(consumer_key + ':' + consumer_secret).toString('base64')
-      },
-      form: {
-          'scope': scope,
-          'grant_type': 'client_credentials'
-      },
-      json: true
-  }
-  var result = await getTokenWait(options);
-  return result;
-}
-
+/**
+ * Do get Z value for single coordinate
+ * 
+ * @function
+ * @name doGetWait
+ * @kind function
+ * @param {any} req - The HTTP request object.
+ * @param {any} res - The HTTP response object.
+ * @param {any} options - The config otions for the axios request.
+ * @returns {void}
+ */
 function doGetWait(req, res, options) {
-  rp(options)
-  .then(function (parsedBody) {
-    // Send the resulting object as json and end response
+  axios(options)
+  .then(response => {
+    const parsedBody = response.data;
     if (srid === '3006') {
       res.send(parsedBody);
     } else {
-      res.send(concatResult(parsedBody, srid));
+      res.send(reProject(parsedBody, srid));
     }
   })
   .catch(function (err) {
-    console.log(err);
-    console.log('ERROR doGetWait!');
-    res.send({'error': err.error });
+    console.error('ERROR doGetWait!', err.message);
+    res.status(500).send({ 'error': err.message });
   });
 }
 
+/**
+ * Do post a GeoJSON geometry and respond with the geometry with Z values
+ * 
+ * @function
+ * @name doGetGeoemtryWait
+ * @kind function
+ * @param {any} req - The HTTP request object.
+ * @param {any} res - The HTTP response object.
+ * @param {any} options - The config otions for the axios request.
+ * @returns {void}
+ */
 function doGetGeoemtryWait(req, res, options) {
-  rp(options)
-  .then(function (parsedBody) {
-    // Send the resulting object as json and end response
+  axios(options)
+  .then(response => {
+    const parsedBody = response.data;
     if (srid === '3006') {
       res.send(parsedBody);
     } else {
-      res.send(concatResult(parsedBody, srid, 'LineString'));
+      res.send(reProject(parsedBody, srid, 'LineString'));
     }
   })
   .catch(function (err) {
-    console.log(err);
-    console.log('ERROR doGetGeoemtryWait!');
-    res.send({'error': err.error });
+    console.error('ERROR doGetGeoemtryWait!', err.message);
+    res.status(500).send({ 'error': err.message });
   });
 }
 
-async function doGetAsyncCall(req, res, configOptions, proxyUrl) {
+/**
+ * Handles elevation data requests, transforming coordinates as necessary and making API calls.
+ *
+ * @param {Object} req - The HTTP request object.
+ * @param {Object} res - The HTTP response object.
+ * @param {Object} configOptions - Configuration options for the API.
+ * @param {string} token - Authorization token for the API.
+ */
+async function doGetAsyncCall(req, res, configOptions, token) {
   var xcoord;
   var ycoord;
   var urlArr;
 
+  // Extracting coordinates and srid from the URL
   urlArr = req.url.split('/');
   if (urlArr[3] !== '') {
     srid = decodeURI(urlArr[3]);
@@ -117,8 +116,10 @@ async function doGetAsyncCall(req, res, configOptions, proxyUrl) {
   if (ycoord.includes('?')) {
     ycoord = ycoord.substring(0, ycoord.indexOf('?'));
   }
+  // POST: Processing posted geometry to get elevation data
   if (req.method == 'POST') { // A geometry was posted so sed it to API to get its Z values
     let bodyContent = req.body;
+    // Re-project coordinates if necessary
     if (srid !== '3006') {
       const newCoordinates = [];
       bodyContent.coordinates.forEach((coord) => {
@@ -134,13 +135,12 @@ async function doGetAsyncCall(req, res, configOptions, proxyUrl) {
         headers: {
           'content-type': 'application/json',
           'Authorization': `Bearer ${token}`,
-          'scope': `${scope}`,
+          'scope': `${configOptions.scope}`,
           'Accept': `application/json`
         },
-        body: bodyContent,
-        json: true // Automatically parses the JSON string in the response
+        data: bodyContent
     }
-
+    // Await the geometry processing request
     await doGetGeoemtryWait(req, res, options);
   } else if (isNaN(srid) || isNaN(xcoord) || isNaN(ycoord) ) {  // Check that request url has numbers
     console.log('ERROR Request parameters not numbers!');
@@ -151,6 +151,7 @@ async function doGetAsyncCall(req, res, configOptions, proxyUrl) {
       console.log('ERROR Wrong crs input, must be between 3006 and 3018 or 3857, 4326!');
       res.send({});
     } else {
+      // Possibly perform coordinate re-projection
       if (srid !== '3006') {
         var newCoordinates = transformCoordinates(srid, '3006', [Number(xcoord), Number(ycoord)]);
         xcoord = newCoordinates[0];
@@ -164,17 +165,27 @@ async function doGetAsyncCall(req, res, configOptions, proxyUrl) {
           headers: {
             'content-type': 'application/json',
             'Authorization': `Bearer ${token}`,
-            'scope': `${scope}`
-          },
-          json: true // Automatically parses the JSON string in the response
+            'scope': `${configOptions.scope}`
+          }
       }
-
+      // Await the simple elevation request
       await doGetWait(req, res, options);
     }
   }
 }
 
-function concatResult(feature, toProjection, type = '') {
+/**
+ * Perform re-projection on GeoJSON
+ * 
+ * @function
+ * @name reProject
+ * @kind function
+ * @param {any} feature
+ * @param {any} toProjection
+ * @param {string} type?
+ * @returns {{ type: string; crs: { type: string; properties: { name: string; }; }; geometry: { type: string; coordinates: any; } | { type: string; coordinates: any[]; }; properties: { ...; }; }}
+ */
+function reProject(feature, toProjection, type = '') {
   const result = {};
   let geometryType = 'Point';
   if (type !== '') {
@@ -210,3 +221,6 @@ function concatResult(feature, toProjection, type = '') {
 
   return result;
 }
+
+// Export the module
+module.exports = lmElevation;
