896ab4482f16db957bc3b32da6f311eb664a7616
Johnny Blästa <johnny.blasta@sundsvall.se>
2024-12-18 11:08:09 +0100

    feature: backend implementing attachment server interface

--- conf/config.js
+++ conf/config.js
@@ -148,5 +148,8 @@ module.exports = {
     grant_type: 'client_credentials',
     scope: 'am_application_scope default',
     query: '{"feature.typ": {"eq": "detaljplan"}, "detaljplan.objektidentitet": {"eq": "$planid$"}, "detaljplan.status": {"in": ["laga kraft"]}}'
+  },
+  attachment: {
+    filepath: "C:\\attachment\\"
   }
 }
